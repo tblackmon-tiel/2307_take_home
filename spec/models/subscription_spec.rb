bbfa1b34af260626753dedb771432b5f83ff024d
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-11-25 12:47:43 -0700

    add model testing for customers, subscriptions and teas

---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Subscription, type: :model do
+  describe "relationships" do
+    it {should belong_to :customer}
+    it {should belong_to :tea}
+  end
+
+  describe "validations" do
+    it {should validate_presence_of :customer_id}
+    it {should validate_presence_of :tea_id}
+    it {should validate_presence_of :price}
+    it {should validate_presence_of :status}
+    it {should validate_presence_of :frequency}
+  end
+end
